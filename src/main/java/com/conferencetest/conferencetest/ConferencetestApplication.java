6d94db5b74a6902cd22e7cfa8bb334da7d5abd2e
Fatemeh Jafari <fatemehjafari020@gmail.com>
2022-11-10 13:42:13 +0400

    ..

---
+++ src/main/java/com/conferencetest/conferencetest/ConferencetestApplication.java
@@ -0,0 +1,13 @@
+package com.conferencetest.conferencetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conferencetest/conferencetest/ConferencetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferencetest.conferencetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
